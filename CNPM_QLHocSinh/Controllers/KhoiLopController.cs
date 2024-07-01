155195b6d7b2267c181accc8fd6ee1546a502749
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-01 17:53:15 +0700

    Thêm tính năng Xem khối lớp

--- CNPM_QLHocSinh/Controllers/KhoiLopController.cs
+++ CNPM_QLHocSinh/Controllers/KhoiLopController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CNPM_QLHocSinh.Models;
 
 namespace CNPM_QLHocSinh.Controllers
 {
     public class KhoiLopController : Controller
     {
+        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: KhoiLop
         public ActionResult Index()
         {
@@ -18,7 +20,7 @@ namespace CNPM_QLHocSinh.Controllers
             => View();
 
         public ActionResult XemKhoiLop()
-            => View();
+            => View(db.KhoiLop);
 
         public ActionResult ChinhSuaKhoiLop()
             => View();
