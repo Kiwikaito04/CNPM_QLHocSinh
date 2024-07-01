f20b72d5abd2849f1fb9598bc4ac2a59a069c144
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-01 10:44:20 +0700

    Fix bug database model

---
+++ CNPM_QLHocSinh/Models/ViewModels/ViewLopHoc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CNPM_QLHocSinh.Models.ViewModels
+{
+    public class ViewLopHoc
+    {
+        public string MaLop { get; set; }
+        public string TenLop { get; set; }
+        public string MoTa { get; set; }
+        public string MaKL { get; set; }
+        public string TenKL { get; set; }
+    }
+}
