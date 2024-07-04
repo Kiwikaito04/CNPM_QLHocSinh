e36c5314d478a9243f66e3bc422af7b363948a02
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-04 22:07:25 +0700

    add ViewModel: ViewMonHoc.cs add View ThemDanhMucMonHoc.cshtml

---
+++ CNPM_QLHocSinh/Models/ViewModels/ViewMonHoc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CNPM_QLHocSinh.Models.ViewModels
+{
+    public class ViewMonHoc
+    {
+        public string MaMH { get; set; }
+        public string TenMH { get; set; }
+        public string MoTa { get; set; }
+    }
+}
