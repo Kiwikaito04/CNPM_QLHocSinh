8b5e15699ca6d3998cc5808540b1ba0378a4ec82
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-07 21:47:17 +0700

    - Update HocSinhController +) Update TimHocSinh +) Thêm chức năng để có thể Thêm Học Sinh +) Thêm chức năng để có thể Xem chi tiết Thông Tin Học Sinh +) Thêm chức năng để có thể Chỉnh sửa thông tin học sinh +) Thêm chức năng để có thể Xóa Học Sinh - Fix Text...

--- CNPM_QLHocSinh/Controllers/HocSinhController.cs
+++ CNPM_QLHocSinh/Controllers/HocSinhController.cs
@@ -15,21 +15,77 @@ namespace CNPM_QLHocSinh.Controllers
 
         public ActionResult ThemHocSinh()
             => View();
+        [HttpPost]
+        public ActionResult ThemHocSinh(HocSinh _hocsinh)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.HocSinh.Add(_hocsinh);
+                    db.SaveChanges();
+                }
+                catch
+                {
+                    ViewBag.Error = "Something went wrong, please try again later";
+                    return View(_hocsinh);
+                }
+                return RedirectToAction("TimHocSinh");
+            }
+            ViewBag.ModelError = "Wrong";
+            return View(_hocsinh);
+        }
 
         public ActionResult TimHocSinh()
             => View(db.HocSinh.ToList());
 
-        public ActionResult XemThongTinHocSinh()
-            => View();
+        public ActionResult XemThongTinHocSinh(string id)
+            => View(db.HocSinh.Where(s => s.MaHS == id).FirstOrDefault());
 
         public ActionResult ChinhSuaThongTinHocSinh(string id)
             => View(db.HocSinh.Where(s => s.MaHS == id).FirstOrDefault());
-    
+        [HttpPost]
+        public ActionResult ChinhSuaThongTinHocSinh(string id, HocSinh _hocsinh)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(_hocsinh).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch
+                {
+                    ViewBag.Error = "Something went wrong, please try again later";
+                    return View(_hocsinh);
+                }
+                return RedirectToAction("TimHocSinh");
+            }
+            ViewBag.ModelError = "Biểu mẫu không đúng";
+            return View(_hocsinh);
+        }
+
         public ActionResult DieuChinhTrangThaiHocSinh()
             => View();
 
-        public ActionResult XoaHocSinh()
-            => View();
+        public ActionResult XoaHocSinh(string id)
+            => View(db.HocSinh.Where(s => s.MaHS == id).FirstOrDefault());
+        [HttpPost]
+        public ActionResult XoaHocSinh(string id, HocSinh _hocsinh)
+        {
+            try
+            {
+                _hocsinh = db.HocSinh.Where(s => s.MaHS == id).FirstOrDefault();
+                db.HocSinh.Remove(_hocsinh);
+                db.SaveChanges();
+            }
+            catch
+            {
+                ViewBag.Error = "Something went wrong, please try again later";
+                return View(_hocsinh);
+            }
+            return RedirectToAction("TimHocSinh");
+        }
 
     }
 }
