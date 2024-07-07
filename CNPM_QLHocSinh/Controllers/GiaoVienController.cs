eaae2e1139a13d832cf7c5a191bb7aceea8adc5c
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-07 22:29:05 +0700

    - Update GiaoVienController +) Thêm tính năng Chỉnh Sửa Thông Tin Giáo Viên +) Update thêm cho tính năng Xem Giáo Viên Index +) Update thêm cho tính năng Xem Thông Tin Giáo Viên +) Thêm tính năng Xóa Giáo Viên

--- CNPM_QLHocSinh/Controllers/GiaoVienController.cs
+++ CNPM_QLHocSinh/Controllers/GiaoVienController.cs
@@ -21,6 +21,7 @@ namespace CNPM_QLHocSinh.Controllers
         public ActionResult ThemGiaoVien()
         {
             ViewBag.ChucVuList = new SelectList(db.ChucVu, "MaCV", "TenCV");
+            //return View();
             return View();
         }
         [HttpPost]
@@ -46,13 +47,53 @@ namespace CNPM_QLHocSinh.Controllers
             return View(_giaoVien);
         }
 
+
         public ActionResult XemThongTinGiaoVien(string id)
             => View(db.GiaoVien.Where(s => s.MaGV == id).Include(s => s.ChucVu).FirstOrDefault());
 
-        public ActionResult ChinhSuaThongTinGiaoVien()
-            => View();
-        public ActionResult XoaGiaoVien()
-            => View();
+        public ActionResult ChinhSuaThongTinGiaoVien(string id)
+            => View(db.GiaoVien.Where(s => s.MaGV == id).Include(s => s.ChucVu).FirstOrDefault());
+        [HttpPost]
+        public ActionResult ChinhSuaThongTinGiaoVien(string id, GiaoVien _giaoVien)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Entry(_giaoVien).State = System.Data.Entity.EntityState.Modified;
+                    db.SaveChanges();
+                }
+                catch
+                {
+                    ViewBag.Error = "Something went wrong, please try again later";
+                    ViewBag.ChucVuList = new SelectList(db.ChucVu, "MaCV", "TenCV");
+                    return View(_giaoVien);
+                }
+                return RedirectToAction("Index");
+            }
+            ViewBag.ModelError = "Biểu mẫu không đúng";
+            ViewBag.ChucVuList = new SelectList(db.ChucVu, "MaCV", "TenCV");
+            return View(_giaoVien);
+        }
+        public ActionResult XoaGiaoVien(string id)
+            => View(db.GiaoVien.Where(s => s.MaGV == id).Include(s => s.ChucVu).FirstOrDefault());
+        [HttpPost]
+        public ActionResult XoaGiaoVien(string id, GiaoVien _giaoVien)
+        {
+            try
+            {
+                _giaoVien = db.GiaoVien.Where(s => s.MaGV == id).FirstOrDefault();
+                db.GiaoVien.Remove(_giaoVien);
+                db.SaveChanges();
+            }
+            catch
+            {
+                ViewBag.Error = "Something went wrong, please try again later";
+                ViewBag.ChucVuList = new SelectList(db.ChucVu, "MaCV", "TenCV");
+                return View(_giaoVien);
+            }
+            return RedirectToAction("Index");
+        }
 
     }
 }
