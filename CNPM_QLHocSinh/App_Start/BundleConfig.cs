d16630b4f6e2fe695bbed1ec83e120e7bb2e129d
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-15 00:22:03 +0700

    Cấu hình lại BundleConfig để dùng Css

--- CNPM_QLHocSinh/App_Start/BundleConfig.cs
+++ CNPM_QLHocSinh/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace CNPM_QLHocSinh
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/mycss.css"));
         }
     }
 }
