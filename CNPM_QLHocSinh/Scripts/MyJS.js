bdf86805295f361e0297fc74f0dd119c794e226d
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-26 16:21:34 +0700

    Chuyển xử lý giới tính vào script

---
+++ CNPM_QLHocSinh/Scripts/MyJS.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var gioiTinhCheckbox = document.getElementById('GioiTinhCheckbox');
+    var gioiTinhLabel = document.getElementById('GioiTinhLabel');
+
+    updateGioiTinhLabel();
+
+    gioiTinhCheckbox.addEventListener('change', function () {
+        updateGioiTinhLabel();
+    });
+
+    function updateGioiTinhLabel() {
+        if (gioiTinhCheckbox.checked) {
+            gioiTinhLabel.textContent = 'Nam';
+        } else {
+            gioiTinhLabel.textContent = 'Nữ';
+        }
+    }
+});
