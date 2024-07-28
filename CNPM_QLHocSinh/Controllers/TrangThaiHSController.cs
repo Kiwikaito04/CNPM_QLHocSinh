2c4fbe7b6b28bf2f014a905dd13ccbeeb949ab4b
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-28 15:55:03 +0700

    Tạo controller CaHoc, ChucVu, TrangThaiHS

---
+++ CNPM_QLHocSinh/Controllers/TrangThaiHSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNPM_QLHocSinh.Controllers
+{
+    public class TrangThaiHSController : Controller
+    {
+        // GET: TrangThaiHS
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
