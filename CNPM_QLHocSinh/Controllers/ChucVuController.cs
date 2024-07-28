aa92f141a5c4e652561fb0505013447da74cba15
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-28 19:51:40 +0700

    Hoàn tất chức vụ

--- CNPM_QLHocSinh/Controllers/ChucVuController.cs
+++ CNPM_QLHocSinh/Controllers/ChucVuController.cs
@@ -9,31 +9,11 @@ namespace CNPM_QLHocSinh.Controllers
 {
     public class ChucVuController : Controller
     {
-        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
+        private readonly CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: ChucVu
         public ActionResult Index()
         {
             return View(db.ChucVu);
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        public ActionResult Edit()
-        {
-            return View();
-        }
-
-        public ActionResult Details()
-        {
-            return View();
-        }
-
-        public ActionResult Delete()
-        {
-            return View();
         }
     }
 }
