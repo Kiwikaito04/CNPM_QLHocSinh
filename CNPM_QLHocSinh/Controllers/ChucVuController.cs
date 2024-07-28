f53bcf42c88083892c0a116a6e452bd07b284df5
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-28 16:31:53 +0700

    View ChucVu

--- CNPM_QLHocSinh/Controllers/ChucVuController.cs
+++ CNPM_QLHocSinh/Controllers/ChucVuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CNPM_QLHocSinh.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,29 @@ namespace CNPM_QLHocSinh.Controllers
 {
     public class ChucVuController : Controller
     {
+        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: ChucVu
         public ActionResult Index()
+        {
+            return View(db.ChucVu);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
         {
             return View();
         }
