085cbacea9ba63afbe91bd1831f59dd543921674
Kiwikaito <nguyenphamdieu41@gmail.com>
2024-07-28 19:55:50 +0700

    Hoàn tất trạng thái học sinh

--- CNPM_QLHocSinh/Controllers/TrangThaiHSController.cs
+++ CNPM_QLHocSinh/Controllers/TrangThaiHSController.cs
@@ -9,31 +9,11 @@ namespace CNPM_QLHocSinh.Controllers
 {
     public class TrangThaiHSController : Controller
     {
-        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
+        private readonly CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: TrangThaiHS
         public ActionResult Index()
         {
             return View(db.TrangThaiHS);
-        }
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        public ActionResult Edit()
-        {
-            return View();
-        }
-
-        public ActionResult Details()
-        {
-            return View();
-        }
-
-        public ActionResult Delete()
-        {
-            return View();
         }
 
     }
