ddc749a442558f66f4a9fd2c62818a964ea33da2
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-28 16:31:16 +0700

    View CaHoc

--- CNPM_QLHocSinh/Controllers/CaHocController.cs
+++ CNPM_QLHocSinh/Controllers/CaHocController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CNPM_QLHocSinh.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,29 @@ namespace CNPM_QLHocSinh.Controllers
 {
     public class CaHocController : Controller
     {
+        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: CaHoc
         public ActionResult Index()
+        {
+            return View(db.CaHoc);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
         {
             return View();
         }
