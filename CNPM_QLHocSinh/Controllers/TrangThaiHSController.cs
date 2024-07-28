ac244577d61b35ed9fcbd3edd9a321f88739a464
TheDuc4869 <22dh110825@st.huflit.edu.vn>
2024-07-28 16:31:02 +0700

    View TrangThaiHS

--- CNPM_QLHocSinh/Controllers/TrangThaiHSController.cs
+++ CNPM_QLHocSinh/Controllers/TrangThaiHSController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CNPM_QLHocSinh.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,32 @@ namespace CNPM_QLHocSinh.Controllers
 {
     public class TrangThaiHSController : Controller
     {
+        CNPM_QLHocSinhEntities db = new CNPM_QLHocSinhEntities();
         // GET: TrangThaiHS
         public ActionResult Index()
         {
+            return View(db.TrangThaiHS);
+        }
+
+        public ActionResult Create()
+        {
             return View();
         }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
     }
 }
